10025d495d05968f7dc10e0c753771df93f8bd63
UnluckyNinja <1270155+UnluckyNinja@users.noreply.github.com>
2022-06-04 20:48:55 +0000

    cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/UnluckyNinja/mangavuer"
       target="_blank"
       title="GitHub"
     />
